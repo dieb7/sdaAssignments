1bd1b2e104ee4193e696df60aa1e9f6aeb715ca7
Diego Benavides <diegob41@gmail.com>
2017-10-08 17:46:06 -0600

    Added User class

---
+++ ood/week4/SharingApp/app/src/main/java/com/example/sharingapp/User.java
@@ -0,0 +1,45 @@
+package com.example.sharingapp;
+
+/**
+ * Created by diebm on 08/10/17.
+ */
+
+public class User {
+    private String username;
+    private String email;
+    private String id;
+
+    public User(String username, String email, String id) {
+        this.username = username;
+        this.email = email;
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void updateId(String id) {
+        setId(id);
+    }
+}
